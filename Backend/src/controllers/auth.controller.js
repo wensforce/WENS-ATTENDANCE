a84560b107e917fa01e94cb962c14c3ce67278b3
Syed-Talmin <saiyedsufyan7@gmail.com>
2026-04-07 19:30:19 +0530

    fix: update SameSite attribute for cookies in login method for improved security

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -63,13 +63,13 @@ export const login = async (req, res) => {
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "strict" : "none", // for testing, change to "strict" in production // for testing, change to "strict" in production
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // for testing, change to "strict" in production // for testing, change to "strict" in production
       maxAge: 15 * 60 * 1000, // 15 minutes
     });
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "strict" : "none", // for testing, change to "strict" in production // for testing, change to "strict" in production
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // for testing, change to "strict" in production // for testing, change to "strict" in production
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
